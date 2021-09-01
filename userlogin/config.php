35523d926a3e6848cbac2bf1627486e8a7bd8722
Kristel Juen <kristeljuen@gmail.com>
2021-09-02 01:06:09 +0800

    mycodesample

--- userlogin/config.php
+++ userlogin/config.php
@@ -8,13 +8,12 @@ $host = "remotemysql.com"
 $db = "E0bML7Ol6o";
 $user = "E0bML7Ol6o";
 $pass = "IrEZ7MoixR";
-$charset = "utf8mb4";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+//$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 
-$db = new PDO($dsn,$user,$pass);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user,$db_pass);
+//$db = new PDO($dsn,$user,$pass);
 //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$db = new PDO('mysql:host=' . $host . ';dbname=' . $db_name . ';charset=utf8', $db_user,$db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
