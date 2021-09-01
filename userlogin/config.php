7919508d109ce835f2f74a6d2d29cd5918b20c9c
Kristel Juen <kristeljuen@gmail.com>
2021-09-02 01:00:50 +0800

    update added remote connection

--- userlogin/config.php
+++ userlogin/config.php
@@ -1,8 +1,20 @@
 <?php
 
-$db_user = "root";
-$db_pass = "";
-$db_name = "useraccounts";
+//$db_user = "root";
+//$db_pass = "";
+//$db_name = "useraccounts";
 
-$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user,$db_pass);
+$host = "remotemysql.com"
+$db = "E0bML7Ol6o";
+$user = "E0bML7Ol6o";
+$pass = "IrEZ7MoixR";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+$db = new PDO($dsn,$user,$pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user,$db_pass);
+//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
